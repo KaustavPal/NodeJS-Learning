1b539d63a77ac26c8fabf95006dfba91d532948c
KaustavPal <kaustavpal95@gmail.com>
2023-11-09 11:49:20 +0530

    arow function to multiply two numbers

--- multiplyTwoNumbers.js
+++ multiplyTwoNumbers.js
@@ -1,7 +1,4 @@
-let num1 = 2;
-let num2 = 8;
-
-const multi = () => {
+const multi = (num1, num2) => {
   return num1 * num2;
 };
-console.log("Product of num1 and num2 is", multi());
+console.log("Product of num1 and num2 is", multi(2, 4));
