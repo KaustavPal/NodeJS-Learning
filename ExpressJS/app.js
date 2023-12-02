ed004aed89eec009efab878afd323f0aacc0ddb9
KaustavPal <kaustavpal95@gmail.com>
2023-12-02 14:21:29 +0530

    Task15 - Starting with MYSQL

--- ExpressJS/app.js
+++ ExpressJS/app.js
@@ -5,6 +5,7 @@ const bodyParser = require("body-parser");
 
 //const rootDir = require("./util/path");
 const error404Controller = require("./controllers/404");
+const db = require("./util/dayabase");
 
 const app = express();
 
@@ -12,6 +13,8 @@ const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 const contactusRoutes = require("./routes/contactus");
 const successRoutes = require("./routes/success");
+
+db.execute("SELECT * FROM products");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
